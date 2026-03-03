564c0509b99a3c723e09e73caaedc517b3fdc781
Nabill-Mulan-Tasya <nabillamulan0@gmail.com>
2026-03-03 19:36:22 +0700

    Laravel API Autentikasi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Tambahkan ini untuk Sanctum API (stateless) 
+        $middleware->api(prepend: [ \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // Kalau butuh SPA auth (cookie + token) 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Api\UserApiController;
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\ProdukController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('users', UserApiController::class);
 Route::apiResource('books', BookController::class);
 Route::apiResource('produks', ProdukController::class);
 Route::apiResource('orders', OrderController::class);
 Route::put('orders/{id}/status', [OrderController::class, 'updateStatus']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        // Contoh: return data user yang login
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']); // Optional: revoke token
+    // Tambah route API lain di sini, misal Route::apiResource('posts', PostController::class);
+});
