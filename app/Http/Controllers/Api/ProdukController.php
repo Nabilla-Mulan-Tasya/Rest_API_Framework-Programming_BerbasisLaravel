fba50b384c7ca696482ef56fb79f1ca181bdf570
Nabill-Mulan-Tasya <nabillamulan0@gmail.com>
2026-05-02 14:32:43 +0700

    update produk controller

--- app/Http/Controllers/Api/ProdukController.php
+++ app/Http/Controllers/Api/ProdukController.php
@@ -47,24 +47,25 @@ class ProdukController extends Controller
         $query->latest();
     }
 
-    $produk = $query->paginate(10);
+    
+    $produk = $query->get();
 
     return response()->json([
         'success' => true,
         'message' => 'List Produk',
-        'data' => ProdukResource::collection($produk),
-        'pagination' => [
-            'current_page' => $produk->currentPage(),
-            'last_page' => $produk->lastPage(),
-            'per_page' => $produk->perPage(),
-            'total' => $produk->total(),
-            'from' => $produk->firstItem(),
-            'to' => $produk->lastItem(),
-            'first_page_url' => $produk->url(1),
-            'last_page_url' => $produk->url($produk->lastPage()),
-            'next_page_url' => $produk->nextPageUrl(),
-            'prev_page_url' => $produk->previousPageUrl(),
-        ]
+        'data' => ProdukResource::collection($produk)
+        // // 'pagination' => [
+        // //     'current_page' => $produk->currentPage(),
+        //     'last_page' => $produk->lastPage(),
+        //     'per_page' => $produk->perPage(),
+        //     'total' => $produk->total(),
+        //     'from' => $produk->firstItem(),
+        //     'to' => $produk->lastItem(),
+        //     'first_page_url' => $produk->url(1),
+        //     'last_page_url' => $produk->url($produk->lastPage()),
+        //     'next_page_url' => $produk->nextPageUrl(),
+        //     'prev_page_url' => $produk->previousPageUrl(),
+        // ]
     ], 200);
 }
 
